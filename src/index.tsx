a04a3cd840b866bffaf16928b439c007d59aa575
pluckabee <claire.patterson@gmail.com>
2022-11-21 14:39:17 +0000

    Add router to index

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+import { fetchGaugeData } from './services/gaugeDataService' 
+
+
+import './App.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    loader: () => fetchGaugeData(),
+    element: <App />,
+    errorElement: <div>Error</div>,
+    id: "app",
+  },
+]);
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+     <RouterProvider router={router} />
   </React.StrictMode>
 );
 
